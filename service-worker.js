d28b9dc607a34ae79ccacb089d07f84fdfa6c36a
bouielouis73-creator <bouielouis73@gmail.com>
2025-10-10 17:53:24 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('lbizzo-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/firebase.js'
-      ]);
-    })
-  );
+// Basic PWA caching for offline
+const CACHE_NAME = "lbizzo-vape-cache-v1";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./firebase.js",
+  "./script.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE)));
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
