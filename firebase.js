cfb7a3cf9fee4c1028a830ed024113d62aab255b
bouielouis73-creator <bouielouis73@gmail.com>
2025-10-10 17:47:58 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,16 @@
+// Firebase config (from your project)
 const firebaseConfig = {
   apiKey: "AIzaSyAMSTyqnUMfyaNMEusapADjoCqSYfjZCs",
   authDomain: "lbizzodelivery.firebaseapp.com",
   projectId: "lbizzodelivery",
-  storageBucket: "lbizzodelivery.appspot.com",
+  storageBucket: "lbizzodelivery.firebasestorage.app",
   messagingSenderId: "614540837455",
   appId: "1:614540837455:web:42709d7b585bbdc2b8203a"
 };
 
 firebase.initializeApp(firebaseConfig);
-window.db = firebase.firestore();
-window._storage = firebase.storage();
-console.log("🔥 Firebase connected successfully");
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+// Firestore collection for products
+window.LBIZZO_COLLECTION = "products";
