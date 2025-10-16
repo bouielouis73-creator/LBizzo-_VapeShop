4046fada0e0ba97e4616689c4a3c34f99279cdf4
bouielouis73-creator <bouielouis73@gmail.com>
2025-10-15 21:21:42 -0400

    Fix formatting of storage variable assignment

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAMSTyqnUMfyaNMEusapADjoCqSYfjZCs",
   authDomain: "lbizzodelivery.firebaseapp.com",
   projectId: "lbizzodelivery",
-  storageBucket: "lbizzodelivery.appspot.com", // ✅ Correct
+  storageBucket: "lbizzodelivery.appspot.com",
   messagingSenderId: "614540837455",
   appId: "1:614540837455:web:42709d7b585bbdc2b8203a",
   databaseURL: "https://lbizzodelivery-default-rtdb.firebaseio.com"
