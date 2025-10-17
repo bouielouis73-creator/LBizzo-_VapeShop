e65e8971947afb949818df8d84710ffc48b88a28
bouielouis73-creator <bouielouis73@gmail.com>
2025-10-17 15:53:26 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,12 @@
-// Firebase config
+// Firebase config (LBizzo Delivery)
 const firebaseConfig = {
   apiKey: "AIzaSyAMSTyqnUMfyaNMEusapADjoCqSYfjZCs",
   authDomain: "lbizzodelivery.firebaseapp.com",
   projectId: "lbizzodelivery",
-  storageBucket: "lbizzodelivery.appspot.com",
+  storageBucket: "lbizzodelivery.firebasestorage.app",
   messagingSenderId: "614540837455",
   appId: "1:614540837455:web:42709d7b585bbdc2b8203a"
 };
 firebase.initializeApp(firebaseConfig);
 window.db = firebase.firestore();
 window.storage = firebase.storage();
-
-window.getStorageURL = async function (path) {
-  try {
-    if (!path.startsWith("products/")) path = "products/" + path;
-    return await storage.ref(path).getDownloadURL();
-  } catch (e) {
-    console.warn("Image error:", path, e);
-    return "https://via.placeholder.com/150?text=No+Image";
-  }
-};
