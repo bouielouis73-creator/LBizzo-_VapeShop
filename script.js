385a1b8abb2ca6da24a2e57568496a3947fd984f
bouielouis73-creator <bouielouis73@gmail.com>
2025-10-18 23:57:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 (() => {
   if (window.__LBIZZO_LOADED__) return;
   window.__LBIZZO_LOADED__ = true;
+// ======= FIREBASE CONFIG =======
+const firebaseConfig = {
+  apiKey: "AIzaSyAMSTyqnUMfyaNMEusapADjoCqSYfjZCs",
+  authDomain: "lbizzodelivery.firebaseapp.com",
+  projectId: "lbizzodelivery",
+  storageBucket: "lbizzodelivery.appspot.com",
+  messagingSenderId: "614540837455",
+  appId: "1:614540837455:web:42709d7b585bbdc2b8203a",
+  databaseURL: "https://lbizzodelivery-default-rtdb.firebaseio.com"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
   // ======= CONSTANTS =======
   const COLLECTION = "products";
   const STORAGE_FOLDER = "products";
