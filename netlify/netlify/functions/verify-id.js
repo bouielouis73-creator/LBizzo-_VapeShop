6a829697f93f13f55173a7f7abb87cd2c291d70f
bouielouis73-creator <bouielouis73@gmail.com>
2025-10-20 15:30:44 -0400

    Update verify-id.js

--- netlify/netlify/functions/verify-id.js
+++ netlify/netlify/functions/verify-id.js
@@ -1,7 +1,1 @@
-// netlify/functions/verify-id.js
-export async function handler(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true, message: "verify-id is working" })
-  };
-}
+
